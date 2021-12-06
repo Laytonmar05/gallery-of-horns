6aae963268b9b58646b27f2d6306fc210f4edbfd
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-12-06 07:52:54 -0800

    hornedBeast

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import beastData from './beastData.json';
 
 class Main extends React.Component {
     render() {
         return (
-        <>
-            <p>Horned Beasts coming soon...</p>
-            <HornedBeast />
-        </>
+            <>
+                <p>Horned Beasts coming soon...</p>
+                {beastData.map((beast, idx) => (
+                    <HornedBeast key={idx} />
+                ))}
+
+            </>
         );
     }
 }
