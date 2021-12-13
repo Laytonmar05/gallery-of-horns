781616c1e3358c11dbc82b4d98fb386c4d495b19
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-12-12 23:52:31 -0800

    bootstrap form

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import "./FilteredForm.css"
+import Form from "react-bootstrap/Form";
 
 class FilterForm extends React.Component {
     render() {return(
 <>
-<form className="form">
-<label for="numOfHorns">Filter by Horns</label>
+<Form className="form" class="form">
+<Form.Label for="numOfHorns">Filter by Horns</Form.Label>
 <br/>
-<select className="formSelect" name="numOfHorns">
+<Form.Select className="formSelect" name="numOfHorns">
 <option>ALL THE HORNS!</option>
-  <option>One Horn</option>
-  <option>Two Horns</option>
-  <option>Three Horns</option>
-  <option>More Horns Than I Can Count!</option>
+  <option value="1">One Horn</option>
+  <option value="2">Two Horns</option>
+  <option value="3">Three Horns</option>
+  <option value="100">More Horns Than I Can Count!</option>
 
-</select>
-</form>
+</Form.Select>
+</Form>
 </>
     ) };
 };
