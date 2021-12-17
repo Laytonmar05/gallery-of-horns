ac09176f76d62b04f58c5a6baf7345b662d25464
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-12-17 08:24:43 -0800

    Filter Code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer'
-import HornedBeast from './HornedBeast';
+
 
 class App extends React.Component {
   render() {
